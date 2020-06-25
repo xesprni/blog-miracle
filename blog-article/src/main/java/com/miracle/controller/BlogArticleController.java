6c96a5db8045e7e601b940c17b31ecf1658a0b05
Miracle <isseidong@outlook.com>
2020-06-25 22:56:03 +0800

    add cache

--- blog-article/src/main/java/com/miracle/controller/BlogArticleController.java
+++ blog-article/src/main/java/com/miracle/controller/BlogArticleController.java
@@ -10,6 +10,8 @@ import com.miracle.model.ModelResult;
 import com.miracle.model.ModelResultClient;
 import com.miracle.model.PageResult;
 import com.miracle.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +23,20 @@ import java.util.Objects;
  * @author Miracle
  * @date 13:32 2020/6/7
  */
+@Slf4j
 @RestController
 public class BlogArticleController implements IArticleService {
 
     private final ArticleService articleService;
+
 
     public BlogArticleController(ArticleService articleService) {
         this.articleService = articleService;
     }
 
     @Override
-    public ModelResult<PageResult<ArticlePO>> getArticleList(@RequestBody(required = false) @Valid ArticleQueryVO articleQueryVO) {
-        if (Objects.isNull(articleQueryVO)) {
-            articleQueryVO = new ArticleQueryVO();
-        }
+    @Cacheable(value = "indexCache", key = "#articleQueryVO.pageNum")
+    public ModelResult<PageResult<ArticlePO>> getArticleList(@RequestBody @Valid ArticleQueryVO articleQueryVO) {
         PageResult<ArticlePO> result = articleService.getArticleList(articleQueryVO);
         if (Objects.nonNull(result)) {
             return new ModelResultClient<PageResult<ArticlePO>>().success(result);
@@ -50,7 +52,7 @@ public class BlogArticleController implements IArticleService {
     @Override
     public ModelResult<Boolean> saveArticle(@RequestBody @Valid ArticleDetailVO articleDetailVO) {
         ArticleDetailPO po = BeanUtil.copyProperties(articleDetailVO, ArticleDetailPO.class);
-        if (articleService.saveArticle(po)==1) {
+        if (articleService.saveArticle(po) == 1) {
             return new ModelResultClient<Boolean>().success(true);
         }
         return new ModelResultClient<Boolean>().fail();
---
+++ blog-gateway/src/main/java/com/miracle/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.miracle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Miracle
+ * @date 11:54 2020/6/25
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
